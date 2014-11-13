ce4f30e5a28867fead1e0e937fe23c48b41fcfd0
kevinyeun <kevinyeun@berkeley.edu>
2014-11-13 00:38:03 -0800

    Fixed particle, added tests

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -7,9 +7,14 @@ struct vec3 {
 	{
 		memset(this, 0, sizeof(vec3));
 	}
-
+	
 	vec3(float x, float y, float z):
-		x(x), y(y), z(z){};
+		x(x), y(y), z(z){}
+
+	vec3(const vec3 &other)
+	{
+		memcpy(this, &other, sizeof(vec3));
+	}
 };
 
 union Particle {
@@ -19,19 +24,21 @@ union Particle {
 		vec3 acc;
 		float density, pressure;
 	};
-
+	/* we need this?
 	struct {
 		float pos_x, pos_y, pos_z;
 		float vel_x, vel_y, vel_z;
 		float acc_x, acc_y, acc_z;
 		float density, pressure;
-	};
+	}*/
 
 	Particle()
 	{
-		pos = vec3();
-		vel = vec3();
-		acc = vec3();
-		density = pressure = 0;
-	};
+		memset(this, 0, sizeof(Particle));
+	}
+
+	Particle(const Particle &other)
+	{
+		memcpy(this, &other, sizeof(Particle));
+	}
 };
