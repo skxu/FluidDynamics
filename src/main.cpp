5b0e1352810c3c5ce70be03dfaceb1c44a66c375
SAAAAAAAAAM <skx@berkeley.edu>
2014-11-14 17:30:04 -0800

    output particles

--- src/main.cpp
+++ src/main.cpp
@@ -4,34 +4,61 @@
 #include <sstream>
 #include <string>
 
-int Initialize(std::vector<Particle*>* particles);
-float PARTICLE_DENSITY = 10.0;
-float PARTICLE_PRESSURE = 11.0;
+#define pVec std::vector<Particle*>
+
+#define DEFAULT_DENSITY 10.0
+#define DEFAULT_PRESSURE 11.0
+#define DEFAULT_TIMESTEPS 300
+
+float partilce_density = DEFAULT_DENSITY;
+float particle_pressure = DEFAULT_PRESSURE;
+int timesteps = DEFAULT_TIMESTEPS;
+
+int Initialize(pVec* particles);
+
 int main(int argc, char* argv[])
 {
-	if (argc == 2)
-	{
-		PARTICLE_PRESSURE = atof(argv[1]);
-		PARTICLE_DENSITY = atof(argv[2]);
-	}
-	
 
-	std::vector<Particle*> particles;
+
+	if (argc == 3)
+	{
+		particle_pressure = atof(argv[1]);
+		partilce_density = atof(argv[2]);
+		timesteps = atoi(argv[3]);
+	}
+
+	pVec particles;
 	if (Initialize(&particles) == -1)
 	{
 		//failed initialize
 		return -1;
 	}
-
 	
 	Solver solver = Solver(particles);
-	
-	
+	std::ofstream output;
+	output.open("../outputs/output.txt");
+	for (int timestep=0; timestep<timesteps; timestep++) 
+	{
+		pVec currentParticles = solver.GetParticles();
+		for (int idx=0; idx<currentParticles.size(); idx++)
+		{
+			Particle* p = currentParticles[idx];
+			output << "Particle ";
+			output << p->pos.x;
+			output << " ";
+			output << p->pos.y;
+			output << " ";
+			output << p->pos.z;
+			output <<"\n";
+		}
+		output << "time\n";
+	}
+
 	return 0;
 
 }
 
-int Initialize(std::vector<Particle*>* particles)
+int Initialize(pVec* particles)
 {
 	std::ifstream inFile("../inputs/init.txt");
 	if (!inFile.is_open()) { return -1; }
@@ -51,9 +78,11 @@ int Initialize(std::vector<Particle*>* particles)
 		Particle* p = new Particle();
 		p->pos = vec3(x, y, z);
 		p->vel = vec3(0.0, 0.0, 0.0);
+
+
 		p->acc = vec3(0.0, 0.0, 0.0);
-		p->density = PARTICLE_DENSITY;
-		p->pressure = PARTICLE_PRESSURE;
+		p->density = partilce_density;
+		p->pressure = particle_pressure;
 		particles->push_back(p);
 	} 
 	return 0;
