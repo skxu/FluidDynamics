4d9fbe7bc22cea7ed9db1a564701219f61374879
Julia Oh <juliahhh.oh@gmail.com>
2014-11-20 19:29:02 -0800

    can we get dinner

--- src/PressureDensity.cpp
+++ src/PressureDensity.cpp
@@ -1,33 +1,40 @@
 #include "PressureDensitySolver.h"
+#include "Constants.h"
+#include "PolynomialKernel.h"
 
-float PDSolver::calcDensity(Particle* p)
+float PDSolver::calcDensity(Particle* p, float h)
 {
-	float rho = 0.0f;
-	fVec distances;
-	first = WeightSolver::CalculateWeights(distances);
-	for (int i = 0; i < masses->size(); i++) {
-		rho += (*first)[i] * (*masses)[i];
-	}
 
+	float rho = 0.0f;
+
+	for (int i=0; i< p->neighbors->size(); i++){
+		float mass = p->neighbors[i]->mass;
+		vec3 direction = p->neighborsDirections[i];
+		rho += p->neighbors[i]->mass * PolynomialKernel::Value(direction, h);
+	}
 	return rho;
 }
 
 float PDSolver::calcPressure(Particle* p)
 {
-	fVec* p = new fVec();
 	float pressure = 0.0f;
-	for each (float density in (*densities)) {
-		pressure = k * (density - rho);
-		p->push_back(pressure);
-	}
-
-	return p;
+	float c_s = SPEED_SOUND * SPEED_SOUND;
+	pressure += c_s * (p->density - DENSITY_WATER);
+	return pressure;
 }
 
-pVec* PDSolver::UpdateDensities(pVec* particle)
+pVec* PDSolver::UpdateDensities(pVec* particle, float h)
 {
+	for (int i=0; i<particle->size(); i++){
+		float d = PDSolver::calcDensity((*particle)[i], h)
+		(*particle)[i]->density = d;
+	}
 }
 
 pVec* PDSolver::UpdatePressures(pVec* particle)
 {
+	for (int i=0; i<particle->size(); i++){
+		float p = PDSolver((*particle)[i]);
+		(*particle)[i]->pressure = p;
+	}
 }
