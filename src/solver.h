2db6e41ff76c15f09b2577c7d8aa341ac14be3c6
kevinyeun <kevinyeun@berkeley.edu>
2014-11-20 18:39:04 -0800

    Update solver

--- src/solver.h
+++ src/solver.h
@@ -5,7 +5,6 @@
 #include "Particle.h"
 #include <vector>
 #include <map>
-#include "WeightSolver.h"
 #include "Grid.h"
 
 class Solver
@@ -15,12 +14,6 @@ private:
 	Grid* grid;
 
 
-
-
-	Particle* UpdateDensityPressure(Particle* particle, pVec* neighborList,
-		fVec weights);
-
-	pVec* CalcNeighbors(Particle* particle);
 
 	//do we need this?
 	void SetParticles(pVec* particleList);
@@ -33,11 +26,6 @@ public:
 	pVec GetParticles();
 
 	void UpdateAll();
-	float calcDensityField(fVec* first, fVec* masses);
-	fVec* calcPressure(float k, float rho, fVec* densities);
-	vec3* calcPressure(vec3 vec, fVec distances, float p, fVec pressures, fVec masses, float rho);
-	vec3* calcViscosity(vec3 vec, fVec distances, float mu, vec3 vel, float rho, float m);
-	float l2Norm(vec3 vec);
 };
 
 
