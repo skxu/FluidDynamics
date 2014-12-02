161be0415c2e5722733cc01994c57074b22e5293
kevinyeun <kevinyeun@berkeley.edu>
2014-12-01 23:59:00 -0800

    make particles stop at hardcoded walls

--- src/ParticleMover.cpp
+++ src/ParticleMover.cpp
@@ -17,6 +17,38 @@ pVec* ParticleMover::updatePositions(pVec* particles, float ts)
 	{
 		Particle* p = (*particles)[i];
 		p->pos += p->vel * ts;
+
+
+// temporary: reflect on normal to floor's plane nvm just zero out
+		if (p->pos.z < 0)
+		{
+			p->pos.z = 0;
+			p->vel.z = 0;
+		}
+
+		if (p->pos.y < 0)
+		{
+			p->pos.y = 0;
+			p->vel.y = 0;
+		}
+
+		if (p->pos.y > 10)
+		{
+			p->pos.y = 10;
+			p->vel.y = 0;
+		}
+
+		if (p->pos.x < 0)
+		{
+			p->pos.x = 0;
+			p->vel.x = 0;
+		}
+
+		if (p->pos.x > 10)
+		{
+			p->pos.x = 10;
+			p->vel.x = 0;
+		}
 	}
 	return particles;
 }
