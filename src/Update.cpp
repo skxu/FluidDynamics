72f073b7769dab14895a8937b3513cdcd8fb085b
piplup15 <alwong8@berkeley.edu>
2014-12-02 11:30:28 -0800

    Leapfrog and collision detection

--- src/Update.cpp
+++ src/Update.cpp
@@ -63,7 +63,7 @@ void compute_accel(sim_state_t* state, sim_param_t* params)
   // Interaction force calculation
   for (int i = 0; i < n; ++i) {
     const float rhoi = rho[i];
-    // To do - grid based to avoid the O(n^2) loop
+    // To do - grid based to avoid the O(n^2) loop, add back Wesley's and Omer's code
     for (int j = i+1; j < n; ++j) {
       float dx = x[3*i+0] - x[3*j+0];
       float dy = x[3*i+1] - x[3*j+1];
@@ -87,5 +87,99 @@ void compute_accel(sim_state_t* state, sim_param_t* params)
         a[3*j+2] -= (wp*dz + wv*dvz);
       }
     }
+  }
+}
+
+void leapfrog_step(sim_state_t* s, double dt)
+{
+  const float* a = s->a;
+  float* vh      = s->vh;
+  float* v       = s->v;
+  float* x       = s->x;
+  int n          = s->n;
+
+  for (int i = 0; i < 3*n; ++i) {
+    vh[i] += a[i] * dt;
+  }
+  for (int i = 0; i < 3*n; ++i) {
+    v[i] = vh[i] + a[i] * dt / 2;
+  }
+  for (int i = 0; i < 3*n; ++i) {
+    x[i] += vh[i] * dt;
+  }
+
+  // Handle reflection across edge and bottom
+  reflect_bc(s);
+}
+
+// Special edge case for first timestep
+void leapfrog_start(sim_state_t* s, double dt)
+{
+  const float* a = s->a;
+  float* vh      = s->vh;
+  float* v       = s->v;
+  float* x       = s->x;
+  int n          = s->n;
+  for (int i = 0; i < 3*n; ++i) {
+    vh[i] += v[i] + a[i] * dt / 2;
+  }
+  for (int i = 0; i < 3*n; ++i) {
+    v[i] = a[i] * dt;
+  }
+  for (int i = 0; i < 3*n; ++i) {
+    x[i] += vh[i] * dt;
+  }
+
+  // Handle reflection across edge and bottom
+  reflect_bc(s);
+}
+
+// Handles boundary collissions
+// dim = 0 (x), dim = 1 (y), dim = 2 (z)
+void damp_reflect(int dim, float barrier,
+                  float* x, float* v, float* vh) {
+  const float DAMP = 0.75;
+
+  // Particle doesn't move -- no update
+  if (v[dim] == 0) return;
+
+  // Scale back distance traveled based on time of collision
+  float tbounce = (x[dim] - barrier)  / v[dim];
+  x[0] -= v[0]*(1-DAMP)*tbounce;
+  x[1] -= v[1]*(1-DAMP)*tbounce;
+  x[2] -= v[2]*(1-DAMP)*tbounce;
+
+  // Reflect position and velocity
+  x[dim]  = 2*barrier - x[dim];
+  v[dim]  = -v[dim];
+  vh[dim] = -vh[dim];
+
+  // Damp the velocities
+  v[0]  *= DAMP;
+  vh[0] *= DAMP;
+  v[1]  *= DAMP;
+  vh[1] *= DAMP;
+  v[2]  *= DAMP;
+  vh[2] *= DAMP;
+}
+
+void reflect_bc(sim_state_t* s) {
+  const float XMIN = 0.0;
+  const float XMAX = 1.0;
+  const float YMIN = 0.0;
+  const float YMAX = 1.0;
+  const float ZMIN = 0.0;
+  // No ZMAX for open top container
+
+  float* vh = s->vh;
+  float* v  = s->v;
+  float* x  = s->x;
+  int n     = s->x;
+  for (int i = 0; i < n; i++, x+=3, v+=3, vh+=3) {
+    if (x[0] < XMIN) damp_reflect(0, XMIN, x, v, vh);
+    if (x[0] > XMAX) damp_reflect(0, XMAX, x, v, vh);
+    if (y[0] < YMIN) damp_reflect(1, YMIN, x, v, vh);
+    if (y[0] > YMAX) damp_reflect(1, YMAX, x, v, vh);
+    if (z[0] < ZMIN) damp_reflect(2, ZMIN, x, v, vh);
   }
 }
--- src/Update.h
+++ src/Update.h
@@ -3,3 +3,11 @@
 
 void compute_density(sim_state_t* s, sim_param_t* params);
 void compute_accel(sim_state_t* state, sim_params_t* params);
+
+void leapfrog_step(sim_state_t* s, double dt);
+void leapfrog_start(sim_state_t* s, double dt);
+
+void damp_reflect(int which, float barrier,
+                  float* x, float* v, float* vh);
+
+void reflect_bc(sim_state_t* s);
