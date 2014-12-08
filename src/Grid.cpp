0b983b39d6edabfd75792aff5cbef664fbbd8faf
piplup15 <alwong8@berkeley.edu>
2014-12-07 19:51:53 -0800

    Add grid

--- src/Grid.cpp
+++ src/Grid.cpp
@@ -75,6 +75,7 @@ void Grid::fitOctopus(int i) {
 
 /* Set neighbors for all particles */
 void Grid::setNeighbors() {
+	#pragma omp parallel for schedule(dynamic, 10)
 	for (int particleInd = 0; particleInd < n; particleInd++)
 	{
 		vector<int>* nVec = neighbors[particleInd];
