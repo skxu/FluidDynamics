cd667e034183a2b8dd9aaacdc3696b3f55bccd17
kevinyeun <kevinyeun@berkeley.edu>
2014-12-07 20:16:06 -0800

    double block

--- src/Grid.cpp
+++ src/Grid.cpp
@@ -82,11 +82,125 @@ void Grid::setNeighbors() {
 		{
 			int neighbor_grid_index = speedOctopus[gridCell][a];
 
-			for (int b = 0; b < grid[gridCell].size(); b++)
+			int b = 0;
+			for (; b < grid[gridCell].size(); b += 2)
 			{
 				int particleInd = grid[gridCell][b];
 				vector<int>* nVec = neighbors[particleInd];
 				__m128 pPos = _mm_load_ps(posVec + 4 * particleInd);
+
+				int particleInd2 = grid[gridCell][b + 1];
+				vector<int>* nVec2 = neighbors[particleInd2];
+				__m128 pPos2 = _mm_load_ps(posVec + 4 * particleInd2);
+
+				int c = 0;
+				for (; c + 4 <= grid[neighbor_grid_index].size(); c += 4)
+				{
+					//int other_particle_index = grid[neighbor_grid_index][c];
+
+					/* DISTANCE CALCULATION */
+					static float CUTOFFVAL = cutoff * cutoff;
+
+					__m128 oPos1 = _mm_load_ps(posVec + 4 * grid[neighbor_grid_index][c]);
+					__m128 oPos2 = _mm_load_ps(posVec + 4 * grid[neighbor_grid_index][c + 1]);
+					__m128 oPos3 = _mm_load_ps(posVec + 4 * grid[neighbor_grid_index][c + 2]);
+					__m128 oPos4 = _mm_load_ps(posVec + 4 * grid[neighbor_grid_index][c + 3]);
+
+					__m128 dif1 = _mm_sub_ps(pPos, oPos1);
+					__m128 dif2 = _mm_sub_ps(pPos, oPos2);
+					__m128 dif3 = _mm_sub_ps(pPos, oPos3);
+					__m128 dif4 = _mm_sub_ps(pPos, oPos4);
+
+					__m128 dist1 = _mm_mul_ps(dif1, dif1);
+					__m128 dist2 = _mm_mul_ps(dif2, dif2);
+					__m128 dist3 = _mm_mul_ps(dif3, dif3);
+					__m128 dist4 = _mm_mul_ps(dif4, dif4);
+
+					__m128 dist = _mm_hadd_ps(_mm_hadd_ps(dist1, dist2), _mm_hadd_ps(dist3, dist4));
+
+					float vals[4] __attribute__((aligned(0x10000)));
+
+					_mm_store_ps(vals, dist);
+
+					/* END DISTANCE CALCULATION*/
+
+					for (int i = 0; i < 4; i++)
+					{
+						if (vals[i] < CUTOFFVAL) {
+							nVec->push_back(grid[neighbor_grid_index][c + i]);
+							//dVec->push_back(vals[i]);
+						}
+					}
+
+
+					dif1 = _mm_sub_ps(pPos2, oPos1);
+					dif2 = _mm_sub_ps(pPos2, oPos2);
+					dif3 = _mm_sub_ps(pPos2, oPos3);
+					dif4 = _mm_sub_ps(pPos2, oPos4);
+
+					dist1 = _mm_mul_ps(dif1, dif1);
+					dist2 = _mm_mul_ps(dif2, dif2);
+					dist3 = _mm_mul_ps(dif3, dif3);
+					dist4 = _mm_mul_ps(dif4, dif4);
+
+					dist = _mm_hadd_ps(_mm_hadd_ps(dist1, dist2), _mm_hadd_ps(dist3, dist4));
+
+					_mm_store_ps(vals, dist);
+
+					for (int i = 0; i < 4; i++)
+					{
+						if (vals[i] < CUTOFFVAL) {
+							nVec2->push_back(grid[neighbor_grid_index][c + i]);
+							//dVec->push_back(vals[i]);
+						}
+					}
+				}
+				for (; c < grid[neighbor_grid_index].size(); c++)
+				{
+					int other_particle_index = grid[neighbor_grid_index][c];
+
+					/* DISTANCE CALCULATION */
+					static float CUTOFFVAL = cutoff * cutoff;
+
+					__m128 oPos = _mm_load_ps(posVec + 4 * other_particle_index);
+
+					__m128 dif = _mm_sub_ps(pPos, oPos);
+
+					__m128 dist = _mm_mul_ps(dif, dif);
+
+					float vals[4] __attribute__((aligned(0x10000)));
+
+					_mm_store_ps(vals, dist);
+					float d = vals[0] + vals[1] + vals[2];
+
+					/* END DISTANCE CALCULATION*/
+					if (d < CUTOFFVAL) {
+						nVec->push_back(other_particle_index);
+						//dVec->push_back(d);
+					}
+
+					dif = _mm_sub_ps(pPos2, oPos);
+
+					dist = _mm_mul_ps(dif, dif);
+
+					_mm_store_ps(vals, dist);
+					float d = vals[0] + vals[1] + vals[2];
+
+					/* END DISTANCE CALCULATION*/
+					if (d < CUTOFFVAL) {
+						nVec2->push_back(other_particle_index);
+						//dVec->push_back(d);
+					}
+
+				}
+			}
+			
+			for (; b < grid[gridCell].size(); b++)
+			{
+				int particleInd = grid[gridCell][b];
+				vector<int>* nVec = neighbors[particleInd];
+				__m128 pPos = _mm_load_ps(posVec + 4 * particleInd);
+				
 				int c = 0;
 				for (; c+4 <= grid[neighbor_grid_index].size(); c+=4)
 				{
